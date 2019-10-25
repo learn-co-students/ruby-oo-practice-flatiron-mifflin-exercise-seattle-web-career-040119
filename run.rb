3053d52039fe710436d11102e1ec09a8e077e3ec
github username <github email address>
2019-10-25 14:43:01 -0700

    Done.

--- run.rb
+++ run.rb
@@ -6,5 +6,15 @@ require 'pry'
 #Test your code here
 
 
-binding.pry
+m1 = Manager.new("Adam", "Accounting", 11)
+m2 = Manager.new("Babs", "Bookkeeping", 22)
+m3 = Manager.new("Carl", "Computing", 33)
+
+e1 = Employee.new("Xavier", 10000, m1)
+e2 = Employee.new("Yanick", 20000, m2)
+e3 = Employee.new("Yves", 20001, m2)
+e4 = Employee.new("Zack", 30000, m3)
+
+
+Pry.start
 puts "done"
